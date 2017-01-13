afc2a3f1ed6e76c8721b0df5d478455150c48fbe
chilichilimo <afshar.m.95@gmail.com>
2017-01-13 12:46:46 +0000

    adding bruteforcer

--- Attacker's Tests/browser.py
+++ Attacker's Tests/browser.py
@@ -1,7 +1,9 @@
 import requests
 
-r = requests.get('http://192.168.0.42')
+r = requests.get('http://192.168.0.42/controller/controller_login.php?username=maya&password=hi')
+
+print r.status_code
 # ' or'1'='1
 
-for objet in r.headers:
-    print objet +': '+ r.headers[objet]
+# for objet in r.headers:
+#     print objet +': '+ r.headers[objet]
